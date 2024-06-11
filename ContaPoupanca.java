32317821e1c7e8eadc3d951d4f624d2ad8502bfc
Leonardo de Vasconcelos <113256127+LELEOU@users.noreply.github.com>
2024-06-10 21:43:54 -0300

    Create ContaPoupanca.java

---
+++ ContaPoupanca.java
@@ -0,0 +1,10 @@
+public class ContaPoupanca extends ContaBancaria {
+    public ContaPoupanca(int numero, String titular, double Saldo) {
+        super(titular, Saldo);
+    }
+
+    public void exibirSaldo() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'exibirSaldo'");
+    }
+}
